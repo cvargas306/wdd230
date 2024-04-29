39393bd7e520fb4dfd90ffc231673757ed5568bd
cvargas306 <cavbcr@gmail.com>
2024-04-29 13:37:52 -0600

    JS added

---
+++ scripts/getDates.js
@@ -0,0 +1,8 @@
+
+const currentYear = new Date().getFullYear();
+
+document.getElementById('currentYear').textContent = currentYear;
+
+const lastModifiedDate = document.lastModified;
+
+document.getElementById('lastModified').textContent = `Last modified: ${lastModifiedDate}`;
