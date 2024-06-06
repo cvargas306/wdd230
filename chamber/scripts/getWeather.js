ba17fa51bf26af78872a012338a3e78df306c389
cvargas306 <cavbcr@gmail.com>
2024-06-05 21:34:53 -0600

    chamber scripts added

---
+++ chamber/scripts/getWeather.js
@@ -0,0 +1,31 @@
+const currentTemp = document.querySelector("#current-temp");
+const weatherIcon = document.querySelector("#weather-icon");
+const captionDescription = document.querySelector("figcaption");
+
+const url = "https://api.openweathermap.org/data/2.5/weather?lat=9.90&lon=-83.99&appid=f214752ad2e32fd55a4071f3f8409886"
+
+async function getWeatherData(url) {
+    try {
+        const respons = await fetch(url);
+        if (respons.ok) {
+
+        } else {
+            throw Error(await respons.text());
+        }
+        
+        const data = await respons.json();
+        console.log(data);
+        displayResults(data);
+    } catch (error) {
+        console.log(error);
+    }    
+}
+
+function displayResults(data) {
+    currentTemp.textContent = (data.main.temp - 273).toFixed(2) + currentTemp.textContent;
+    weatherIcon.src = `https://openweathermap.org/img/wn/${data.weather[0].icon}.png`;
+    weatherIcon.alt = "Weather event";
+    captionDescription.textContent = data.weather[0].description;
+}
+
+getWeatherData(url);
